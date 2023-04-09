9d853709b195452a818981517d7e35873d2a9265
Krystal-G <lcs2020017@iiitl.ac.in>
2023-04-09 15:27:30 +0530

    Add controllers for organization routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const {joinDriverToOrganization,joinUserToOrganization,getOrganizationById,createOrganization,getUser,getOrganizations,getRides} = require("../controller/organizations");
+// const auth = require("../middleware/auth");
 
 router.post("/api/auth/signup/driver");
 router.post("/api/auth/signup/user");
 router.post("/api/auth/login");
 
-router.post("/api/organizations/join");
-router.get("/api/organizations/:id");
+router.post("/api/organizations/join/driver",joinDriverToOrganization);
+router.post("/api/organizations/join/user",joinUserToOrganization);
 
-router.post("/api/organizations/create");
-router.get("/api/users/:id"); // user Info
+router.get("/api/organizations/:id",getOrganizationById);
+
+router.post("/api/organizations/create",createOrganization);
+router.get("/api/users/:id",getUser); // user Info
+
 
 router.post("/api/organizations/:id/assignRides");
-router.get("/api/organizations/:id/getRides");
+router.get("/api/organizations/:id/getRides",getRides);
 
-router.get("/api/organizations/");
+router.get("/api/organizations/",getOrganizations);
+
+module.exports = router;
