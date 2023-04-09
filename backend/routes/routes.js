b9e3c53d945caf8d28cba75854afbe01b116cf9e
ManasAg-hub <86635617+ManasAg-hub@users.noreply.github.com>
2023-04-09 15:52:09 +0530

    added routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const {driverSignup,passengerSignup,login} = require("../controller/user");
 
-router.post("/api/auth/signup/driver");
-router.post("/api/auth/signup/user");
-router.post("/api/auth/login");
+const {joinDriverToOrganization, getRides, joinUserToOrganization,getOrganizations} = require("../controller/organizations");
+router.post("/api/auth/signup/driver", driverSignup);
+router.post("/api/auth/signup/user",  passengerSignup);
+router.post("/api/auth/login", login);
 
-router.post("/api/organizations/join");
-router.get("/api/organizations/:id");
+router.post("/api/organizations/join/driver",joinDriverToOrganization);
+router.post("/api/organizations/join/user",joinUserToOrganization);
+router.get("/api/organizations/:id",getOrganizations);
 
 
-router.post("/api/organizations/:id/assignRides");
+router.post("/api/organizations/:id/assignRides", assignRides);
 router.get("/api/organizations/:id/getRides",getRides);
 
 router.get("/api/organizations/",getOrganizations);
 
-router.get("/api/organizations/");
+router.get("/api/organizations/", getOrganizations);
